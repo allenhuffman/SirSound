552d39e2c3a9712c2c9ea5d812f355c524a7c9b8
Allen Huffman <alsplace@pobox.com>
2018-03-29 20:05:23 -0500

    new header file with prototype

---
+++ TonePlayer.h
@@ -0,0 +1,11 @@
+#ifndef TONEPLAYER_H
+#define TONEPLAYER_H
+/*---------------------------------------------------------------------------*/
+
+void tonePlayNote(byte note, unsigned long duration)
+
+#endif // TONEPLAYER_H
+
+/*---------------------------------------------------------------------------*/
+// End of TonePlayer.h
+
